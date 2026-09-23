bb30a5855376b43b06cb7359b3d304abc19eedff
nurbektech <nurbektech@gmail.com>
2026-09-23 18:32:26 +0500

    feat(accounts): create custom user

--- src/config/settings.py
+++ src/config/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # local apps
+    "apps.accounts",
 ]
 
 MIDDLEWARE = [
@@ -121,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = "static/"
-
+AUTH_USER_MODEL = "accounts.User"
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
